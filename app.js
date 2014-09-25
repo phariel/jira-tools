5e3c5923859c20b55a21f6d74567d757cf3d125f
Phariel <phariel@gmail.com>
2014-09-25 20:23:09 +0800

    prototype

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var routeIssue = require('./routes/findissue');
 
 var app = express();
 
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,38 @@ var router = require('express').Router();
 router.get('/', function (req, res) {
 
 	res.render('index', {
-		logined: !!req.session.user
+		logined: !!req.session.logined
 	});
 });
 
 router.post('/login', function (req, res) {
-	console.log(req.body);
-	res.json({
-		'success': 'true'
-	});
+	var username = req.body.username;
+	var password = req.body.password;
+	if (username && password) {
+		jiraHelper.login(username, password).then(function (logined) {
+			req.session.logined = logined;
+			res.json({
+				success: logined
+			});
+		});
+	} else {
+		res.json({
+			success: false
+		});
+	}
+});
+
+router.post('/release/get', function (req, res) {
+	var fixversion = req.body.fixversion;
+	if (fixversion) {
+		jiraHelper.getReleaseList(fixversion).then(function (data) {
+			res.json(data);
+		});
+	} else {
+		res.json({
+			items: null
+		});
+	}
 });
 
 router.get('/issue/:id', function (req, res) {
